6e4ad52c603d9b52c343cb4f4dbd3276f4766ade
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-13 14:15:47 +0300

    Update urls.py

--- apps/jobs/urls.py
+++ apps/jobs/urls.py
@@ -1,19 +1,13 @@
-# apps/jobs/urls.py
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 from .views import JobViewSet, CategoryViewSet, ApplicationViewSet, RegisterView
 
-# Create a router and register your viewsets
 router = DefaultRouter()
 router.register(r"jobs", JobViewSet, basename="jobs")
 router.register(r"categories", CategoryViewSet, basename="categories")
 router.register(r"applications", ApplicationViewSet, basename="applications")
 
-# Define URL patterns
 urlpatterns = [
-    # User registration
     path("auth/register/", RegisterView.as_view(), name="register"),
-    
-    # Include all router URLs (jobs, categories, applications)
     path("", include(router.urls)),
 ]
