f372e549e018d6dc1e6a462e74b3814316a923f9
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-13 11:27:58 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,24 +1,23 @@
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework.permissions import AllowAny
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Job Board API",
         default_version="v1",
-        description="API documentation",
+        description="API documentation for Job Board Backend",
     ),
     public=True,
-    permission_classes=(AllowAny,),
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/token/", TokenObtainPairView.as_view()),
-    path("api/token/refresh/", TokenRefreshView.as_view()),
-    path("api/", include("apps.jobs.urls")),
-    path("api/docs/", schema_view.with_ui("swagger")),
+    path("api/jobs/", include("apps.jobs.urls")),
+    # Swagger docs
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
