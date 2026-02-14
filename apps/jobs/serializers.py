dcb92227b87dc7628880e994a6a3a7f7be38bedb
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-14 11:30:02 +0300

    Update serializers.py

--- apps/jobs/serializers.py
+++ apps/jobs/serializers.py
@@ -46,6 +46,7 @@ class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = "__all__"
+        read_only_fields = ["created_by", "created_at"]
 
 
 # ==========================
