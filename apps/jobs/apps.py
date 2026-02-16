9150b8ef3a1404e83e842c0e90b664ee268bc939
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-02-16 08:44:11 +0300

    Update apps.py

--- apps/jobs/apps.py
+++ apps/jobs/apps.py
@@ -1,6 +1,7 @@
-from django.apps import AppConfig
+# apps/jobs/apps.py
+from django.apps import AppConfig  # <- remove the space
 
 class JobsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.jobs"
-    label = "jobs"
+    label = "jobs"  # optional, can leave it if you want
